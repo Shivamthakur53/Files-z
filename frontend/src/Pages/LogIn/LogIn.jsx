03adcb6b4ae3c4b0ef0db8b9aca294614a18b080
Shivam thakur <shivamthakur176125@gmail.com>
2024-05-01 15:22:00 +0530

    login updated

--- frontend/src/Pages/LogIn/LogIn.jsx
+++ frontend/src/Pages/LogIn/LogIn.jsx
@@ -1,13 +1,45 @@
-import React from "react";
-// import "./Login.css";
+import React, { useState } from "react";
+import "./LogIn.css";
 const LogIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [inputColor, setInputColor] = useState("black");
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+    setInputColor("white");
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    setInputColor("white");
+  };
   return (
     <div>
       <div className="container">
-        <h1>Login</h1>
-        <p>Please login to continue</p>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <h1 className="head">Login</h1>
+        <p className="paragrp">Please login to continue</p>
+        <input
+          type="email"
+          onChange={handleEmailChange}
+          style={{ color: inputColor }}
+          className="emaill"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          onChange={handlePasswordChange}
+          style={{ color: inputColor }}
+          className="passwordd"
+          placeholder="Password"
+        />
+        <div className="additional-info">
+          <div className="chek">
+            <input type="checkbox" />
+            <p>Remember me</p>
+          </div>
+          <p className="forgot-password">Forgot password?</p>
+        </div>
+        <button className="loginbtn">Login</button>
       </div>
     </div>
   );
