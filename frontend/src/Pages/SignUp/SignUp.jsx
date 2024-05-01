a69333b31a33952f1d60662e079ff18b6856dd0a
Anubhav <anubhav27km@gmail.com>
2024-05-01 16:12:54 +0530

    signup updated

--- frontend/src/Pages/SignUp/SignUp.jsx
+++ frontend/src/Pages/SignUp/SignUp.jsx
@@ -1,19 +1,37 @@
 import React from "react";
+import "./SignUp.css";
+import Logo from "../../assets/mainlogo.png";
 
 const SignUp = () => {
   return (
     <>
       <div className="container">
         <div className="row">
+          <div className="img-box">
+            <img src={Logo} alt="" className="logo" />
+          </div>
           <h1>Sign Up</h1>
           <p>Sign up and start managing your Files Online</p>
-          <div className="signup-form">
-            <form action="#">
-              <input type="email" name="email" id="" placeholder="EMAIL" />
-              <input type="text" name="username" id="" placeholder="USERNAME" />
+          <div>
+            <form action="#" className="signup-form">
+              <input
+                type="email"
+                className="email"
+                name="email"
+                id=""
+                placeholder="EMAIL"
+              />
+              <input
+                type="text"
+                className="username"
+                name="username"
+                id=""
+                placeholder="USERNAME"
+              />
               <input
                 type="password"
                 name="password"
+                className="password"
                 id=""
                 placeholder="PASSWORD"
               />
