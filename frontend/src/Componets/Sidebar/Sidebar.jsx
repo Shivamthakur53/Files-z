fb0858c33622e94d6c158b5a125fa16fb0e7b38a
Anubhav <anubhav27km@gmail.com>
2024-05-01 18:22:41 +0530

    updated sidebar

--- frontend/src/Componets/Sidebar/Sidebar.jsx
+++ frontend/src/Componets/Sidebar/Sidebar.jsx
@@ -14,6 +14,26 @@ const Sidebar = () => {
             <ion-icon name="home"></ion-icon>
             <p>Home</p>
           </div>
+          <div className="shared">
+            <ion-icon name="people-outline"></ion-icon>
+            <p>Shared with me</p>
+          </div>
+          <div className="recent">
+            <ion-icon name="time-outline"></ion-icon>
+            <p>Recent</p>
+          </div>
+          <div className="starred">
+            <ion-icon name="star-outline"></ion-icon>
+            <p>Starred</p>
+          </div>
+          <div className="spam">
+            <ion-icon name="alert-circle-outline"></ion-icon>
+            <p>Spam</p>
+          </div>
+          <div className="trash">
+            <ion-icon name="trash-outline"></ion-icon>
+            <p>Trash</p>
+          </div>
         </div>
       </div>
     </>
